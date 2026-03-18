e83cab8201ed7c87b1b8a7aec813af8e0ad4fc9f
yashawant sharma <yashawantsharma785@gmail.com>
2026-03-18 21:11:37 +0530

    add data

--- frontEnd/myapp/src/api/apis.js
+++ frontEnd/myapp/src/api/apis.js
@@ -1,2 +1,2 @@
-const API="https://insurance-i8qy.onrender.com"//"http://localhost:5050";
-export default API;
+const API="https://insurance-665p.onrender.com"||"http://localhost:5050";
+export default API
